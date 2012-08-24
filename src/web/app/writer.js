b3979e75fa53e3e1b6397686aa8a97ca3b57bea1
Sten Roger Sandvik <srs@enonic.com>
2012-08-24 13:16:47 +0200

    Added robohash and logo.

--- src/web/app/writer.js
+++ src/web/app/writer.js
@@ -2,14 +2,16 @@
 function getAvatarUrl(user, size) {
     var email = user.toLowerCase() + "@enonic.com";
     var hash = md5(email);
-    return "http://www.gravatar.com/avatar/" + hash + ".jpg?s=" + size + "&d=monsterid";
+
+    // return "http://www.gravatar.com/avatar/" + hash + ".jpg?s=" + size + "&d=monsterid";
+    return "http://robohash.org/" + hash + ".png?size=" + size + "x" + size + "&set=set1";
 }
 
 function writeLogEntryElements(writeElementId, logEntryElement) {
     var logEntrySource = logEntryElement._source;
     var rowResult = $("#" + writeElementId);
 
-    logEntrySource.avatarUrl = getAvatarUrl(logEntrySource.resource, 50);
+    logEntrySource.avatarUrl = getAvatarUrl(logEntrySource.resource, 60);
 
     var source = $("#log-entry-template").html();
     var result = Mustache.render(source, logEntrySource);
